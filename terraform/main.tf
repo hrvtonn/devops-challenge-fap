0c949b6c4109a39b4b53f17d2a0881f2a4cc8ec8
hrvtonn <heryvelton20@gmail.com>
2026-08-28 19:40:30 -0300

    feat: update terraform to use sa-east-1 and dynamic AMI

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,8 +67,24 @@ resource "aws_security_group" "api_sg" {
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS in us-east-1 (example)
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   
   vpc_security_group_ids = [aws_security_group.api_sg.id]
